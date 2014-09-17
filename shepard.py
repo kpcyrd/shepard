f36cffd31aae6df80ecc9a68b9a773069ea61c86
kpcyrd <git@rxv.cc>
2014-09-17 19:38:36 +0200

    Allow monitoring in infinity loop

--- shepard.py
+++ shepard.py
@@ -4,6 +4,7 @@ import yaml
 from subprocess import call, PIPE
 from argparse import ArgumentParser
 import requests
+import time
 import sys
 
 OK = Fore.GREEN + ' OK ' + Fore.RESET
@@ -39,16 +40,31 @@ def run_all_tests(services):
             status = run_http_test(service['http'])
         print_status(service['host'], service['service'], status)
 
+
+def run_all_tests_forever(services, interval):
+    try:
+        while True:
+            run_all_tests(services)
+            print('~' * 25)
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        pass
+
+
 def main():
     args = parser.parse_args()
 
     with open(args.monitor) as f:
         services = yaml.load(f)
 
-    run_all_tests(services)
+    if args.interval is not None:
+        run_all_tests_forever(services, args.interval)
+    else:
+        run_all_tests(services)
 
 parser = ArgumentParser()
 parser.add_argument('monitor', help='yaml file for monitoring defintions')
+parser.add_argument('-i', '--interval', type=int, help='repeat tests in interval x')
 
 if __name__ == '__main__':
     main()
